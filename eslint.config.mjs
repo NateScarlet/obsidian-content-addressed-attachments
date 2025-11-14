3c0ef70d15b7d30abcea26c289d84b01b7c4c0b4
NateScarlet <NateScarlet@Gmail.com>
2025-11-14 23:39:18 +0800

    chore: configure eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import tsparser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import { defineConfig, globalIgnores } from "eslint/config";
+import obsidianmd from "eslint-plugin-obsidianmd";
+import globals from "globals";
+
+export default defineConfig([
+	globalIgnores(["node_modules/", "main.js"]),
+	...obsidianmd.configs.recommended,
+	{
+		files: ["**/*.ts"],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+			parser: tsparser,
+			parserOptions: {
+				sourceType: "module",
+				project: "./tsconfig.json",
+			},
+		},
+		plugins: {
+			obsidianmd,
+			"@typescript-eslint": tsPlugin,
+		},
+	},
+]);
