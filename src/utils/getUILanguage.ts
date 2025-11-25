f462a2e28fd967fc4e1d5fe273d6d3bcc29edf7a
NateScarlet <NateScarlet@Gmail.com>
2025-11-25 17:02:01 +0800

    refactor: update `getUILanguage`

--- src/utils/getUILanguage.ts
+++ src/utils/getUILanguage.ts
@@ -1,23 +1,22 @@
+import { getLanguage } from "obsidian";
+
 // obsidian 修改语言会要求重启，所以可以缓存
 const cachedResult = (() => {
-	try {
-		// eslint-disable-next-line @typescript-eslint/no-require-imports -- 兼容低版本
-		const obsidian = require("obsidian") as typeof import("obsidian");
-		return obsidian.getLanguage();
-	} catch {
-		if (typeof navigator.languages === "object") {
-			const zhIndex = navigator.languages.findIndex((i) =>
-				i.startsWith("zh"),
-			);
-			const enIndex = navigator.languages.findIndex((i) =>
-				i.startsWith("en"),
-			);
-			if (zhIndex >= 0 && (zhIndex < enIndex || enIndex < 0)) {
-				return "zh";
-			}
-		}
-		return "en";
+	if (typeof getLanguage === "function") {
+		return getLanguage();
 	}
+	if (typeof navigator.languages === "object") {
+		const zhIndex = navigator.languages.findIndex((i) =>
+			i.startsWith("zh"),
+		);
+		const enIndex = navigator.languages.findIndex((i) =>
+			i.startsWith("en"),
+		);
+		if (zhIndex >= 0 && (zhIndex < enIndex || enIndex < 0)) {
+			return "zh";
+		}
+	}
+	return "en";
 })();
 
 export default function getUILanguage(): string {
