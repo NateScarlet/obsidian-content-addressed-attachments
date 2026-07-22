70792ca777197facf82e3f9671eec563a3bd580e
NateScarlet <NateScarlet@Gmail.com>
2026-07-22 15:47:01 +0800

    refactor(types): move obsidian App module augmentation to src/types/obsidian.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -6,43 +6,33 @@ type WellKnownDirectory =
 	| "pictures"
 	| "videos";
 
-declare global {
-	interface Window {
-		electron?: {
-			shell?: {
-				openPath(path: string): void;
-				openExternal(url: string): void;
-			};
+interface Window {
+	electron?: {
+		shell?: {
+			openPath(path: string): void;
+			openExternal(url: string): void;
 		};
-		/** https://developer.mozilla.org/en-US/docs/Web/API/Window/showOpenFilePicker */
-		showOpenFilePicker(options?: {
-			excludeAcceptAllOption?: boolean;
-			id?: string;
-			multiple?: boolean;
-			startIn?: FileSystemFileHandle | WellKnownDirectory;
-			types?: {
-				accept: Record<string, string[]>;
-				description?: string;
-			}[];
-		}): Promise<FileSystemFileHandle[]>;
-		/** https://developer.mozilla.org/en-US/docs/Web/API/Window/showSaveFilePicker */
-		showSaveFilePicker(options?: {
-			excludeAcceptAllOption?: boolean;
-			id?: string;
-			startIn?: FileSystemFileHandle | WellKnownDirectory;
-			suggestedName?: string;
-			types?: {
-				accept: Record<string, string[]>;
-				description?: string;
-			}[];
-		}): Promise<FileSystemFileHandle>;
-	}
+	};
+	/** https://developer.mozilla.org/en-US/docs/Web/API/Window/showOpenFilePicker */
+	showOpenFilePicker(options?: {
+		excludeAcceptAllOption?: boolean;
+		id?: string;
+		multiple?: boolean;
+		startIn?: FileSystemFileHandle | WellKnownDirectory;
+		types?: {
+			accept: Record<string, string[]>;
+			description?: string;
+		}[];
+	}): Promise<FileSystemFileHandle[]>;
+	/** https://developer.mozilla.org/en-US/docs/Web/API/Window/showSaveFilePicker */
+	showSaveFilePicker(options?: {
+		excludeAcceptAllOption?: boolean;
+		id?: string;
+		startIn?: FileSystemFileHandle | WellKnownDirectory;
+		suggestedName?: string;
+		types?: {
+			accept: Record<string, string[]>;
+			description?: string;
+		}[];
+	}): Promise<FileSystemFileHandle>;
 }
-
-declare module "obsidian" {
-	interface App {
-		secretStorage?: import("../lib/encryption/types").KeyStorage;
-	}
-}
-
-export {};
